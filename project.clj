101b9a9661afb26829767fe89abe2d9e0fb68b5f
Andrew Brehaut <andrew@brehaut.net>
2014-03-25 13:00:16 +1300

    including cljs dependancy
    initial plan was to port to cljs but cljs.core.logic is currently
    missing finite domain support.

--- project.clj
+++ project.clj
@@ -3,5 +3,21 @@
   :url "http://github.com/brehaut/manticore"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]
-                 [org.clojure/core.logic "0.8.7"]])
+  :dependencies [#_[org.clojure/clojure "1.5.1"]
+                 [org.clojure/core.logic "0.8.7"]
+                 [org.clojure/clojurescript "0.0-2156"
+                  :exclusions [org.apache.ant/ant]]]
+
+  :source-paths ["src/clj" 
+                 "src/cljs"]
+
+  :cljsbuild {
+    :builds [{
+        :source-paths ["src/cljs"]
+        :compiler {
+          :output-to "target/main.js" 
+          :optimizations :whitespace
+          :pretty-print true}}]}
+
+  :profiles {:dev {:plugins [[lein-cljsbuild "1.0.2"]
+                             [com.cemerick/austin "0.1.4"]]}})
