27686df4b76b2a65bb95ac768b416e0d6a6f840c
Andrew Brehaut <andrew.brehaut@wildbamboo.co.nz>
2016-10-19 12:57:08 +1300

    Results now generated entirely with react

--- src/ts/app/ui/results.ts
+++ src/ts/app/ui/results.ts
@@ -10,154 +10,28 @@ module manticore.ui {
     var _ = strings._;
 
     export class ResultsView {
-        private el: HTMLElement;
-        private resultsEl: HTMLElement;
-        private moreButton: HTMLElement;
-
         public onRequestGenerate = new Event<null>();
 
-        private currentIndex: number;
-        private pendingAllocations:data.GroupedEncounters;
-        
         private reactResults: results.Results;
 
         constructor (private parent:HTMLElement) {
             this.createElements();
-            
-            this._appendTo(this.parent);
-        }
-
-        private _appendTo(el:HTMLElement) {
-            el.appendChild(this.el);
         }
 
         public markResultsAsOutOfDate() {
-            this.resultsEl.classList.add("outofdate");
+            this.reactResults.markResultsAsOutOfDate();
         }
 
         public displayResults(party: data.IParty, allocs: data.GroupedEncounters) {
-            DOM.empty(this.resultsEl);
-            
-            this.resultsEl.appendChild(this.resultsSummary(party, allocs));
-            this.resultsEl.classList.remove("outofdate");
-            
-            this.currentIndex = 0; 
-            
-            this.pendingAllocations = allocs;
-
-            this.show100();
-        }
-
-        private allocationGroupMarkup(encounters: data.Encounters) {
-            if (encounters.length === 1) {
-                return encounters[0].map(al => this.allocationMarkup(al));
-            }   
-            else {
-                const summary:Node[] = encounters[0].map(al => this.allocationMarkup(al, false));
-                summary.push(DOM.ul({"style": "clear:left"}, encounters.map(enc => DOM.li({"class": "clearfix"}, enc.map(al => this.allocationMarkup(al))))));
-
-                return [DOM.div({
-                    "class": "allocation-group",
-                },
-                [
-                    DOM.div({}, summary)
-                ])];
-            }                       
-        }     
-
-        private allocationMarkup(alloc: data.Allocation, showcount = true) {
-            return DOM.div(
-                {"class": "allocation " + alloc.monster.size},
-                [
-                    DOM.div({"class":"kind"},[
-                        DOM.text(_(alloc.monster.kind)),
-                        DOM.span({"class":"level"}, 
-                                 [DOM.text(alloc.monster.level.toString())]),
-                        DOM.span({"class":"book"}, 
-                                 [DOM.text(alloc.monster.book)])
-                    ]),
-                    DOM.em({"class": "name"}, [
-                        DOM.text(_(alloc.monster.name))
-                    ]),
-                    showcount ? DOM.span({"class": "number"}, [
-                        DOM.text(" ×" + alloc.num.toString())
-                    ]) : DOM.span({"class": "number"})
-                ]
-            );                           
-        }       
-
-        private resultsSummary(party: data.IParty, allocs: data.GroupedEncounters) {
-            return DOM.header(
-                {"class": "results-summary"},
-                [
-                    DOM.h1(null, [DOM.text(_("Possible encounters"))]),
-                    DOM.p(null, [DOM.text(template(_("{count} encounters for {num} level {level} characters."),
-                                                   {count: allocs.length,
-                                                    num: party.size,
-                                                    level: party.level}))])
-                ]
-            );
-        }
-
-        private show100() {
-            const window = 100;           
-
-            const allocs = this.pendingAllocations.slice(this.currentIndex, this.currentIndex + window);
-
-            allocs.forEach(group => {
-                this.resultsEl.appendChild(DOM.li(
-                    {"class": "clearfix"}, 
-                    this.allocationGroupMarkup(group)
-                ));
-            });
-
-            this.currentIndex += window;
-
-            if (this.currentIndex > this.pendingAllocations.length) {
-                this.moreButton.style.display = "none";
-            }
-            else {
-                this.moreButton.style.display = "inline-block";
-            }
+            this.reactResults.displayResults(party, allocs);
         }
 
         private createElements() {
-            var clickGenerate = (e) => {
-                this.onRequestGenerate.trigger(null);
-                e.preventDefault();
-                e.stopPropagation();
-            };
-            
-            this.el = ui.sectionMarkup("Encounters", "results", "[results summary]", [
-                DOM.div(
-                    {
-                        "class": "button generate",
-                        
-                        onclick: clickGenerate,
-                    },
-                    [
-                        DOM.text(_("generate encounters"))
-                    ])
-            ]);
-
-            this.resultsEl = DOM.ul({"class": "encounters"}, []);
-            this.moreButton = DOM.div(
-                {
-                    "class":"button",
-
-                    onclick: (e) => this.show100(),
-                }, 
-                [DOM.text(_("more"))]
-            );
-            this.moreButton.style.display = "none";
-
-            this.el.appendChild(this.resultsEl);
-            this.el.appendChild(this.moreButton);
-
-            const container = DOM.div(null);
+            const container = document.createElement("div");
             this.parent.appendChild(container);
 
             this.reactResults = results.installResults(container);
+            this.reactResults.onRequestGenerate.register(_ => this.onRequestGenerate.trigger(null));
         }
     }
 }
--- src/ts/app/ui/results.tsx
+++ src/ts/app/ui/results.tsx
@@ -6,6 +6,50 @@ module manticore.ui.results {
     "use strict";
     import _ = manticore.ui.strings._; 
 
+    class Allocation extends React.Component<{ alloc: data.Allocation, showCount?: boolean }, undefined> {
+        public render() {
+            const alloc = this.props.alloc;
+
+            return (
+                <div className="allocation { alloc.monster.size }">
+                    <div className="kind">
+                        { _(alloc.monster.kind) }
+                        <span className="level">{ alloc.monster.level }</span>
+                        <span className="book">{ alloc.monster.book }</span>
+                    </div>
+                    <em>{ _(alloc.monster.name) }</em>
+                    <span className="number">{ this.props.showCount !== false ? alloc.num : "" }</span>
+                </div>
+            );
+        }
+    }
+
+    class AllocationGroup extends React.Component<{ allocations: data.Encounters }, undefined> {
+        public render() {
+            if (this.props.allocations.length === 1) {
+                return <li className="single-allocation">{ this.props.allocations[0].map(alloc => <Allocation alloc={alloc} />) }</li>;
+            }
+            return (
+                <li>
+                    <div className="allocation-group">
+                        <div>
+                            {this.props.allocations[0].map(alloc => <Allocation alloc={alloc} showCount={false}/>) }
+
+                            <ul style={{clear: "left"}}>
+                                { this.props.allocations.map(enc => 
+                                    <li className="clearfix">
+                                        { enc.map(alloc => <Allocation alloc={alloc} />) }
+                                    </li>
+                                )}
+                            </ul>
+                        </div>
+                    </div>
+                </li>
+            );
+        } 
+    }
+
+
     interface ResultsProps {
         
     }
@@ -18,6 +62,9 @@ module manticore.ui.results {
     }
 
     export class Results extends React.Component<ResultsProps, ResultsState> {
+        public onRequestGenerate = new Event<null>();
+
+
         constructor(props: ResultsProps) {
             super(props);
 
@@ -25,24 +72,55 @@ module manticore.ui.results {
                 stale: true,
                 party: undefined,
                 allocs: [],
+                show: 100,
+            });
+        }
+
+        public displayResults(party: data.IParty, allocs: data.GroupedEncounters) {            
+            this.setState({ 
+                stale: false,
+                party: party,
+                allocs: allocs,
                 show: 100
             });
         }
 
         public markResultsAsOutOfDate() {
+            this.setState({ stale: true } as ResultsState);
         }
 
-        public render() {
+        public render() {           
+            const allocs = this.state && this.state.allocs ? this.state.allocs.slice(0, this.state.show) : [];
+            const party = this.state && this.state.party ? this.state.party : {size: 0, level: 0 };
+
             return (
                 <section className="results">
                     <header>
                         <h1>{ _("Encounters") }</h1>
                         <p>{ _("[results summary]") }</p>
                     </header>
-                    <div className="button generate">{_("generate encounters") }</div>
+                    <div className="button generate" onClick={ (_) => this.generateClicked() }>{_("generate encounters") }</div>
+
+                    <section class="encounters">
+                        <header>
+                            <h1>{ _("Possible encounters") }</h1>
+                            <p>{ template(_("{count} encounters for {num} level {level} characters."),
+                                                   {count: allocs.length,
+                                                    num: party.size,
+                                                    level: party.level}) }
+                            </p>
+                        </header>
+
+                        <ul className="encounters {this.state.stale ? 'outofdate' : ''}">
+                        { allocs.map(alloc => <AllocationGroup allocations={alloc} />) }
+                        </ul>
+                    </section>
                 </section>
             );
+        }
 
+        private generateClicked() {
+            this.onRequestGenerate.trigger(null);
         }
     }
 
