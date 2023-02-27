4b8f18acc2add3bc067608716f9b1576efebdd25
Andrew Brehaut <andrew@brehaut.net>
2023-02-28 11:36:46 +1300

    Alpha status warning

--- src/lib/ui/Edition.svelte
+++ src/lib/ui/Edition.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
     import { Edition } from '$lib/costs/index.js';
+    import Section from './Section.svelte';
     import Toggle from './Toggle.svelte';
 
     export let edition: Edition;
@@ -13,8 +14,14 @@
     <span>Use <cite>13th Age 2e</cite> Alpha rules</span> <Toggle on:change={use2e} enabled={edition === Edition.Second}/>
 </div>
 
+{#if edition === Edition.Second}
+<div class="warning">
+Second edition support is currently in early alpha stages and likely contains bugs
+</div>
+{/if}
+
 <style>
-    div {
+    div:not(.warning) {
         position: absolute;
         top: 1rem;
         right: 2rem;
@@ -22,5 +29,12 @@
 
     span {
         opacity: 0.5;
+    }
+
+    div.warning {
+        background: rgb(255, 255, 176);
+        padding: 1rem;
+        font-weight: bold;
+        text-align: center;
     }
 </style>
